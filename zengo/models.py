665da42f43039c5484b63ffef4a0e4878c095351
Myles Braithwaite <me@mylesbraithwaite.com>
2020-03-10 13:48:46 -0400

    :bug: Fix a bug in the priority.

--- zengo/models.py
+++ zengo/models.py
@@ -84,7 +84,7 @@ class Ticket(models.Model):
         Constant(normal="normal"),
         Constant(low="low"),
     )
-    priority = ConstantChoiceCharField(constants=priorities, max_length=8)
+    priority = ConstantChoiceCharField(constants=priorities, max_length=8, blank=True)
     # custom fields and tags are stored here, relatively unprocessed and
     # are None, or parseable JSON
     custom_fields = models.TextField(null=True, blank=True)
